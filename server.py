8e65403a500b8fc9e3c5271337c2c85898a4735f
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-09-20 19:49:40 -0400

    added requirements and instruction to print the host's IP

---
+++ server.py
@@ -0,0 +1,20 @@
+import flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def program():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 1234))
+    s.listen(5)
+
+    print(socket.gethostname())
+
+    while True:
+        clientsocket, adress = s.accept()
+        print(f"{adress}, has connected")
+        clientsocket.send(bytes("You\'ve been connected to 404\'s computer", "utf-8"))
+
+if __name__ == '__main__':
+    app.run()
