64e2cef58302ff147f0be56caab8c83e1d70ad24
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-09-20 19:59:10 -0400

    worked around duplicate files

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import flask import Flask
-import socket
+from server import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def program():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 1234))
-    s.listen(5)
-
-    while True:
-        clientsocket, adress = s.accept()
-        print(f"{adress}, has connected")
-        clientsocket.send(bytes("You\'ve been connected to 404\'s computer", "utf-8"))
+    runServer() 
 
 if __name__ == '__main__':
     app.run()
--- server.py
+++ server.py
@@ -1,10 +1,6 @@
-import flask import Flask
 import socket
 
-app = Flask(__name__)
-
-@app.route('/')
-def program():
+def runServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((socket.gethostname(), 1234))
     s.listen(5)
@@ -15,6 +11,3 @@ def program():
         clientsocket, adress = s.accept()
         print(f"{adress}, has connected")
         clientsocket.send(bytes("You\'ve been connected to 404\'s computer", "utf-8"))
-
-if __name__ == '__main__':
-    app.run()
