14807e503a7443273dd046ed0750d02ec9ff1a4e
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-09-20 21:02:36 -0400

    -made it so it returns the ip to the webpage for testing purposes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from server import *
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def program():
-    runServer() 
+    return socket.gethostname()
+    //runServer() 
 
 if __name__ == '__main__':
     app.run()
